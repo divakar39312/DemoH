5fc8ba966eb53bac26782e44a605f049bcb9ec6a
Divakar <divakar39312@gmail.com>
2022-08-03 10:45:52 +0530

    [Divakar]: added addition of number

--- src/com/bridgelab/demo/Addition.java
+++ src/com/bridgelab/demo/Addition.java
@@ -4,7 +4,7 @@ public class Addition {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Divakar");
+		System.out.println("Hello Divakarr");
 
 	}
 
